ca147bbe459c3575ed27d29529d44af60c92dc3a
dendomenko <dendomenko@gmail.com>
2018-02-19 12:29:42 +0200

    Finish video tasks. create scenario 1.1. user can create question

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'questions#index'
+
   resources :questions, except: %i[edit update destroy] do
     resources :answers, except: %i[edit update destroy]
   end
