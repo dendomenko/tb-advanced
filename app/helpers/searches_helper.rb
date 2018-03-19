3ad1df490eca1fb6aa22be9de4dc776682073313
Denis Domenko <dendomenko@gmail.com>
2018-03-19 11:13:06 +0200

    Added partials for objects from search

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -2,4 +2,9 @@ module SearchesHelper
   def models
     %w[All User Question Answer Comment]
   end
+
+  def link_to_question(comment)
+    question = comment.commentable.is_a?(Question ) ? comment.commentable : comment.commentable.question
+    link_to 'Comment', question, class: 'badge badge-primary'
+  end
 end
