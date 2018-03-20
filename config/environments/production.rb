fa496685ab89d381362f4629c11e1ac8f091be91
Denis Domenko <dendomenko@gmail.com>
2018-03-20 16:03:30 +0200

    Set up mailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.default_url_options = { host: '54.186.176.148' }
+  config.action_mailer.default_url_options = { :host => 'yourapp.heroku.com' }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
